068eb59d7647cf41f0b61b51975c69d35ae0d4e0
adhelll <99anton01@gmail.com>
2022-10-23 19:15:05 +0300

    kinematic

---
+++ firmware/Torse/include/arm_kinematic.h
@@ -0,0 +1,89 @@
+#ifndef TORSE_ARM_KINEMATIC_H
+#define TORSE_ARM_KINEMATIC_H
+
+#include <math.h>
+#include "part_arm_driver.h"
+#include "common.h"
+#include "macro_lib.h"
+
+/**
+ * @brief   Structures for coordinates. unit of measurement: meter
+ */
+typedef struct{
+    float x;
+    float y;
+    float z;
+}coord_t;
+
+/**
+ * @brief   Structures for angles. unit of measurement: radians
+ */
+typedef struct{
+    float th1;
+    float th2;
+    float th3;
+}angles_t;
+
+/**
+ * @brief   Structures for arm sizes. unit of measurement: meter
+ */
+typedef struct{
+    float shoulder_length;
+    float forearm_length;
+}length_t;
+
+/**
+ * @brief   Structure for hand parameters.
+ * @note    side - calculate a left or right hand, shoulder_length - shoulder
+ *          length in meter(L1), forearm_length - forearm length in meter(L2),
+ *          coord_base - shoulder point coordinates in absolute coordinate system.
+ */
+typedef struct{
+    arm_side_t side;
+    length_t arm_length;
+    coord_t coord_base;
+}arm_kinematic_t;
+
+typedef enum{
+    ERROR_NONE = 0,
+    ERROR_NEGATIVE_DISCRIMINANT = 1,
+    ERROR_FALSE_LENGTH = 2,
+    ERROR_SIDE = 3
+}kinematic_error_t;
+
+/**
+ * @brief   Forward kinematics function.
+ * @details A function that takes three angles and calculates the
+ *          coordinates of the grip point.
+ * @note    The function calculates the position of the grip point
+ *          in the absolute coordinate system, the beginning of which
+ *          is specified in the "arm_t" structure.
+ * @note    Angles are accepted in radians, coordinates are returned
+ *          in meters.
+ *
+ * @param[in] arm     Structure with hand option and parameters,
+ * @param[in] ang     Three angles
+ * @return            Three coordinates
+ */
+
+kinematic_error_t arm_direct_kinematic(arm_kinematic_t* arm, angles_t* angles, coord_t* coord_position);
+
+/**
+ * @brief   Invers kinematics function.
+ * @details A function that takes three coordinates and calculates
+ *          the rotation angle of the links..
+ * @note    The function calculates the angles in the absolute
+ *          coordinate system, the origin of which is specified
+ *          in the "arm_t" structure.
+ * @note    Restrictions are imposed on the angles, if violated, 180
+ *          degrees are returned.
+ * @note    Coordinates are accepted in meters, angles are returned
+ *          in radians.
+ *
+ * @param[in] arm     Structure with hand option and parameters,
+ * @param[in] coord   Three coordinates
+ * @return            Three angles
+ */
+
+kinematic_error_t arm_inverse_kinematic(arm_kinematic_t* arm, coord_t coord, angles_t* arm_angles);
+#endif //TORSE_ARM_KINEMATIC_H
---
+++ firmware/robo_lib/macro_lib.h
@@ -0,0 +1,10 @@
+#ifndef TORSE_MACRO_LIB_H
+#define TORSE_MACRO_LIB_H
+
+#define PI 3.1415926535F
+#define DEG2RAD 0.0174532925F
+#define RAD2DEG 57.2957795131F
+
+#define FROUND(x) (float)(((x)>=0)?(int32_t)((x)+0.5):(int32_t)((x)-0.5))
+
+#endif //TORSE_MACRO_LIB_H
