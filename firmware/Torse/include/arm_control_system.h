8ce690cc109b00841e4c64f79204d1aa532cf21f
adhelll <99anton01@gmail.com>
2023-03-25 13:35:07 +0300

    fix conflicts

--- firmware/Torse/include/arm_control_system.h
+++ firmware/Torse/include/arm_control_system.h
@@ -3,12 +3,12 @@
 
 #include "common.h"
 #include "arm_encoder.h"
-#include "part_arm_driver.h"
+#include "joint.h"
 #include "PID.h"
+#include "macro_lib.h"
 
 void acs_init(joint_t* joint);
 void acs_update_angle(float dt, arm_side_t side, joint_t *joint);
 void acs_set_angle(float target_angle, arm_side_t side, arm_info_t *arm_driver, angle_lim_t* global_angle_lim, float working_interval);
-
 
 #endif
--- firmware/Torse/project.mk
+++ firmware/Torse/project.mk
@@ -1,5 +1,4 @@
 PROJECT_MODULES = src/serial.c \
-
 		  src/lld_control_servo.c \
 		  src/elbow_driver.c \
 		  src/arm_control_system.c \
@@ -19,24 +18,24 @@ PROJECT_MODULES = src/serial.c \
 		  ../robo_lib/flash_f7.c \
 		  ../robo_lib/crc32.c \
 
-
 PROJECT_TESTS   = tests/test_lld_control_servo.c tests/test_nonlinear_servo.c \
-                  tests/test_elbow.c \
-                  tests/test_arm_control_system.c \
-                  tests/test_PID.c \
-                  tests/test_encoder.c \
-                  tests/test_arm_cs.c \
-                  
+		  tests/test_arm_high_function.c \
+		  tests/test_arm_control_system.c \
+		  tests/test_PID.c \
+		  tests/test_encoder.c \
+		  tests/test_arm_kinematic.c \
+
 # C source files
 PROJECT_CSRC 	=  src/main.c \
-					$(PROJECT_MODULES) $(PROJECT_TESTS)			 
+					$(PROJECT_MODULES) $(PROJECT_TESTS)
 # C++ source files
-PROJECT_CPPSRC 	= 
+PROJECT_CPPSRC 	=
 # Directories to search headers in
-PROJECT_INCDIR	= include/
+PROJECT_INCDIR	= include/ \
+				  ../robo_lib/
 # Additional libraries
 PROJECT_LIBS	=
 # Compiler options
-PROJECT_OPT     = -DCHPRINTF_USE_FLOAT=1    
+PROJECT_OPT     = -DCHPRINTF_USE_FLOAT=1
 
 # Additional .mk files are included here
