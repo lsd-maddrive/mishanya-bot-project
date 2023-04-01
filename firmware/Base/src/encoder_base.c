6201aee4bec356aeb91a57c3e6910fe83f832d88
Lenke <seryhelena@gmail.com>
2023-04-01 14:12:35 +0300

    [RF]: initial

--- firmware/Base/src/encoder_base.c
+++ firmware/Base/src/encoder_base.c
@@ -6,29 +6,29 @@ int8_t  status_phase_B = 0;
 
 encoder_t encoder_1 =
 {
-     .line_phase_A       = ENCODER1_PHASE_A,
-     .line_phase_B       = ENCODER1_PHASE_B,
-     .direction_rotation = 0,
-     .tic_count          = 0,
-     .rev_count          = 0
+    .line_phase_A       = ENCODER1_PHASE_A,
+    .line_phase_B       = ENCODER1_PHASE_B,
+    .direction_rotation = 0,
+    .tic_count          = 0,
+    .rev_count          = 0
 };
 
 encoder_t encoder_2 =
 {
-     .line_phase_A       = ENCODER2_PHASE_A,
-     .line_phase_B       = ENCODER2_PHASE_B,
-     .direction_rotation = 0,
-     .tic_count          = 0,
-     .rev_count          = 0
+    .line_phase_A       = ENCODER2_PHASE_A,
+    .line_phase_B       = ENCODER2_PHASE_B,
+    .direction_rotation = 0,
+    .tic_count          = 0,
+    .rev_count          = 0
 };
 
 encoder_t encoder_3 =
 {
-     .line_phase_A       = ENCODER3_PHASE_A,
-     .line_phase_B       = ENCODER3_PHASE_B,
-     .direction_rotation = 0,
-     .tic_count          = 0,
-     .rev_count          = 0
+    .line_phase_A       = ENCODER3_PHASE_A,
+    .line_phase_B       = ENCODER3_PHASE_B,
+    .direction_rotation = 0,
+    .tic_count          = 0,
+    .rev_count          = 0
 };
 
 /**
@@ -57,25 +57,25 @@ void ProcessEncoderData(encoder_t* enc_t)
 {
     uint16_t prev_tick = 0;
     status_phase_B     = palReadLine(enc_t->line_phase_B);
-    if(status_phase_B == 0)
+    if (status_phase_B == 0)
     {
         enc_t->direction_rotation = 1;
         enc_t->tic_count += 1;
     }
-    else if(status_phase_B == 1)
+    else if (status_phase_B == 1)
     {
         enc_t->direction_rotation = 0;
         enc_t->tic_count -= 1;
     }
-    if(abs(enc_t->tic_count) >= MAX_TIC)
+    if (abs(enc_t->tic_count) >= MAX_TIC)
     {
         prev_tick = abs(enc_t->tic_count) - MAX_TIC;
         enc_t->tic_count = prev_tick;
-        if(enc_t->direction_rotation == 1)
+        if (enc_t->direction_rotation == 1)
         {
             enc_t->rev_count += 1;
         }
-        else if(enc_t->direction_rotation == 0)
+        else if (enc_t->direction_rotation == 0)
         {
             enc_t->rev_count -= 1;
         }
@@ -84,19 +84,19 @@ void ProcessEncoderData(encoder_t* enc_t)
 
 void lldEncoderInit(type_encoder encoder_n)
 {
-    if(encoder_n == ENCODER_1)
+    if (encoder_n == ENCODER_1)
     {
         palEnableLineEvent(encoder_1.line_phase_A, PAL_MODE_INPUT_PULLDOWN);
         palEnableLineEvent(encoder_1.line_phase_A, PAL_EVENT_MODE_RISING_EDGE);
         palSetLineCallback(encoder_1.line_phase_A, ExtEnc1, NULL);
     }
-    else if(encoder_n == ENCODER_2)
+    else if (encoder_n == ENCODER_2)
     {
         palEnableLineEvent(encoder_2.line_phase_A, PAL_MODE_INPUT_PULLDOWN);
         palEnableLineEvent(encoder_2.line_phase_A, PAL_EVENT_MODE_RISING_EDGE);
         palSetLineCallback(encoder_2.line_phase_A, ExtEnc2, NULL);
     }
-    else if(encoder_n == ENCODER_3)
+    else if (encoder_n == ENCODER_3)
     {
         palEnableLineEvent(encoder_3.line_phase_A, PAL_MODE_INPUT_PULLDOWN);
         palEnableLineEvent(encoder_3.line_phase_A, PAL_EVENT_MODE_RISING_EDGE);
@@ -107,15 +107,15 @@ void lldEncoderInit(type_encoder encoder_n)
 int16_t GetEncoderRawTicks(type_encoder encoder_n)
 {
     int16_t tc = 0;
-    if(encoder_n == ENCODER_1)
+    if (encoder_n == ENCODER_1)
     {
         tc = encoder_1.tic_count;
     }
-    else if(encoder_n == ENCODER_2)
+    else if (encoder_n == ENCODER_2)
     {
         tc = encoder_2.tic_count;
     }
-    else if(encoder_n == ENCODER_3)
+    else if (encoder_n == ENCODER_3)
     {
         tc = encoder_3.tic_count;
     }
@@ -125,15 +125,15 @@ int16_t GetEncoderRawTicks(type_encoder encoder_n)
 bool GetEncoderDirection(type_encoder encoder_n)
 {
      bool dr = 0;
-     if(encoder_n == ENCODER_1)
+     if (encoder_n == ENCODER_1)
      {
          dr = encoder_1.direction_rotation;
      }
-     else if(encoder_n == ENCODER_2)
+     else if (encoder_n == ENCODER_2)
      {
          dr = encoder_2.direction_rotation;
      }
-     else if(encoder_n == ENCODER_3)
+     else if (encoder_n == ENCODER_3)
      {
          dr = encoder_3.direction_rotation;
      }
@@ -144,17 +144,17 @@ float GetEncoderRawRevs(type_encoder encoder_n)
 {
      float rev = 0;
      encoder_t cur_encoder;
-     if(encoder_n == ENCODER_1)
+     if (encoder_n == ENCODER_1)
      {
-         cur_encoder = encoder_1;
+        cur_encoder = encoder_1;
      }
-     else if(encoder_n == ENCODER_2)
+     else if (encoder_n == ENCODER_2)
      {
-         cur_encoder = encoder_2;
+        cur_encoder = encoder_2;
      }
-     else if(encoder_n == ENCODER_3)
+     else if (encoder_n == ENCODER_3)
      {
-         cur_encoder = encoder_3;
+        cur_encoder = encoder_3;
      }
      rev = getRevs(&cur_encoder);
      return rev;
@@ -169,23 +169,23 @@ float getRevs(encoder_t* cur_encoder)
 
 void ResetEncoder(type_encoder encoder_n)
 {
-     if(encoder_n == ENCODER_1)
+     if (encoder_n == ENCODER_1)
      {
-         encoder_1.direction_rotation = 0;
-         encoder_1.tic_count = 0;
-         encoder_1.rev_count = 0;
+        encoder_1.direction_rotation = 0;
+        encoder_1.tic_count = 0;
+        encoder_1.rev_count = 0;
      }
-     else if(encoder_n == ENCODER_2)
+     else if (encoder_n == ENCODER_2)
      {
-         encoder_2.direction_rotation = 0;
-         encoder_2.tic_count = 0;
-         encoder_2.rev_count = 0;
+        encoder_2.direction_rotation = 0;
+        encoder_2.tic_count = 0;
+        encoder_2.rev_count = 0;
      }
-     else if(encoder_n == ENCODER_3)
+     else if (encoder_n == ENCODER_3)
      {
-         encoder_3.direction_rotation = 0;
-         encoder_3.tic_count = 0;
-         encoder_3.rev_count = 0;
+        encoder_3.direction_rotation = 0;
+        encoder_3.tic_count = 0;
+        encoder_3.rev_count = 0;
      }
 }
 
